f45c2c6c95520ba73b4e0a3f830e3a1b51e33608
gl14916 <garyli14916@live.com>
2016-11-21 16:23:02 -0500

    Beta

---
+++ Assets/Mic.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class Mic : MonoBehaviour
+{
+    private AudioSource aud;// = new AudioSource();
+    private bool micConnected = false;
+    private int minFreq;
+    private int maxFreq;
+    // Use this for initialization
+    void Start()
+    {
+        //Debug.Log(System.Environment.Version);
+        aud = this.GetComponent<AudioSource>();
+        Microphone.GetDeviceCaps(null, out minFreq, out maxFreq);
+        if (Microphone.devices.Length <= 0)
+        {
+            //Throw a warning message at the console if there isn't  
+            Debug.LogWarning("Microphone not connected!");
+        }
+        else //At least one microphone is present  
+        {
+
+            micConnected = true;
+            Microphone.GetDeviceCaps(null, out minFreq, out maxFreq);
+            if (minFreq == 0 && maxFreq == 0)
+            {
+                //...meaning 44100 Hz can be used as the recording sampling rate  
+                maxFreq = 44100;
+            }
+        }
+
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 25, 200, 50), "\n\n\n\n\n\nmaxFreq = " + maxFreq + "\nminFreq = " + minFreq + "\nmicConnected = " + micConnected);
+        //If there is a microphone  
+        if (micConnected)
+        {
+            if (!Microphone.IsRecording(null))
+            {
+                if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), "Record"))
+                {
+                    aud.clip = Microphone.Start(null, true, 10, maxFreq);
+                }
+            }
+            else //Recording is in progress  
+            {
+                if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), "Stop and Play!"))
+                {
+                    Microphone.End(null); //Stop the audio recording  
+                    aud.Play(); //Playback the recorded audio  
+                }
+
+
+                GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 25, 200, 50), "Recording in progress...");
+            }
+        }
+        else
+        {
+            GUI.contentColor = Color.red;
+            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 25, 200, 50), "Microphone not connected!");
+        }
+        if (GUI.Button(new Rect(Screen.width / 2 + 105, Screen.height / 2 - 25, 200, 50), "Save"))
+        {
+            if (File.Exists(@"D:\STT\myfile.wav"))
+                File.Delete(@"D:\STT\myfile.wav");
+            SavWav.Save(@"D:\STT\myfile.wav", aud.clip);
+            Socket.stt(@"D:\STT\myfile.wav");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //aud.Play();
+
+    }
+}
---
+++ Assets/Socket.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class Socket : MonoBehaviour
+{
+
+    //public static string audPath = @"D:\Downloads\myfile.wav";
+    public static string txtPath = @"D:\STT\rcv.txt";
+
+
+
+    [Serializable]
+    public class openingConfMsg
+    {
+        public string action;
+        public string contentXtype;
+        public bool continuous;
+        public bool interim_results;
+    }
+
+    public class closingConfMsg
+    {
+        public string action;
+    }
+
+    public class Alternative
+    {
+        public double confidence { get; set; }
+        public string transcript { get; set; }
+    }
+
+    public class Result
+    {
+        public List<Alternative> alternatives { get; set; }
+        public bool final { get; set; }
+    }
+
+    public class STTResult
+    {
+        public List<Result> results { get; set; }
+        public int result_index { get; set; }
+    }
+
+
+
+
+    public static void stt(string audPath)
+    {
+        string wsURI = "wss://stream.watsonplatform.net/speech-to-text/api/v1/recognize";
+        string username = ""; //credentials from Watson's Speech-to-text console
+        string password = ""; //credentials from Watson's Speech-to-text console
+
+
+
+        FileInfo audSend = new FileInfo(audPath);
+
+        FileInfo txtRcv = new FileInfo(txtPath);
+        if (!txtRcv.Exists)
+        {
+            txtRcv.CreateText();
+        }
+
+
+
+        using (var ws = new WebSocket(wsURI))
+        {
+
+            int a = 1;
+
+
+            ws.SetCredentials(username, password, true);
+
+
+            ws.OnOpen += (sender, e) =>
+            {
+                //string message = "{\"action\": \"start\", \"content-type\": \"audio/wav\", \"continuous\": true, \"interim_results\": true}";
+                openingConfMsg openingMsg = new openingConfMsg();
+                openingMsg.action = "start";
+                openingMsg.contentXtype = "audio/wav";
+                openingMsg.continuous = false;
+                openingMsg.interim_results = false;
+
+                string confmsg = JsonConvert.SerializeObject(openingMsg).Replace("X", "-");
+                ws.Send(confmsg);
+            };
+
+            ws.OnOpen += (sender, e) =>
+            {
+
+                //sending audio clip
+                ws.Send(audSend);
+
+
+                /*FileStream fs = File.OpenRead(audPath);
+                byte[] b = new byte[1024];
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    ws.Send(b);
+                }*/
+            };
+
+            ws.OnOpen += (sender, e) =>
+            {
+
+                //message = "{\"action\": \"stop\"}";
+                closingConfMsg closingMsg = new closingConfMsg();
+                closingMsg.action = "stop";
+
+                string confmsg = JsonConvert.SerializeObject(closingMsg);
+                ws.Send(confmsg);
+
+            };
+
+
+
+
+            ws.OnMessage += (sender, e) =>
+            {
+                if (e.IsText)
+                {
+                    var sttresult = JsonConvert.DeserializeObject<STTResult>(e.Data);
+
+                    foreach (var result in sttresult.results)
+                    {
+                        foreach (var alternative in result.alternatives)
+                        {
+
+                            Debug.Log("Result : " + alternative.transcript);
+                            if (alternative.transcript != null)
+                            {
+                                StreamWriter tmp = txtRcv.AppendText();
+                                tmp.Write(alternative.transcript);
+                                tmp.Write("\n");
+                                Debug.Log("Written to: " + txtPath);
+                                tmp.Close();
+                            }
+                        }
+                    }
+                }
+            };
+
+            ws.OnClose += (sender, e) =>
+            {
+                ws.Close();
+                if (a-- > 0) ws.Connect();
+
+            };
+
+            ws.Connect();
+
+        }
+    }
+
+}
